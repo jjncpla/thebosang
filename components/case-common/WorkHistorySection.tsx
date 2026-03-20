faf9d771eef11447bf5cb0562e7d7b4837d8538b
jjncpla <jjncpla@gmail.com>
2026-03-20 15:06:41 +0900

    fix: sync localRaw with workHistoryRaw prop changes

--- components/case-common/WorkHistorySection.tsx
+++ components/case-common/WorkHistorySection.tsx
@@ -1,6 +1,6 @@
-"use client";
+﻿"use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { WorkHistoryItem, WorkHistoryRaw, WorkHistoryRawEntry, WorkHistoryDailyEntry } from "./WorkHistoryTypes";
 
 interface WorkHistorySectionProps {
